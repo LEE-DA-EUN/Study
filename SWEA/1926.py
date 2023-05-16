033dd0ef55283cfe08eef6b3abeaaf5a14e38c79
delee3064 <delee3064@gmail.com>
2023-05-16 16:24:57 +0900

    369게임 수정

--- SWEA/1926.py
+++ SWEA/1926.py
@@ -14,4 +14,17 @@ for i in range(1, t+1):
     else:
         print(i, end=" ")
 
-# 백의자리 3,6,9일 때 ---인 경우 예외처리 추가해야할듯 테스트케이스는 통과함
+# 백의자리 3,6,9일 때 ---인 경우 추가해야할듯 테스트케이스는 통과함
+
+# 백의자리까지 처리 완료
+# count() 함수를 사용하여 처리할 수 있음
+t = int(input())
+for i in range(1, t+1):
+    s = str(i)
+    num = 0
+    num = s.count("3") + s.count("6") + s.count("9")
+    if num != 0:
+        print("-"*num, end=" ")
+    else:
+        print(i, end=" ")
+    
