3d92aca6fd556e9b6fcc8f54ef5358f3fde418eb
delee3064 <delee3064@gmail.com>
2023-08-03 13:29:17 +0900

    2720

--- BOJ/2720.py
+++ BOJ/2720.py
@@ -1,13 +1,6 @@
 t = int(input())
-q = d = ni = p = 0
 for i in range(t):
     n = int(input())
-    q = n//25
-    n = n%25
-    d = n//10
-    n = n%10
-    ni = n//5
-    p = n%5
-    print(q,d,ni,p)
-
-
+    for j in [25,10,5,1]:
+        print(n//j,end=' ')
+        n = n%j
